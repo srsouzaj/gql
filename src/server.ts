e1af3f8fff6baeea7f4f7fca2204fa3305470224
Jorge de Souza <jorgejrdj1@gmail.com>
2023-09-17 15:24:45 -0300

    feat: schema first model

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,50 @@
 import { gql, ApolloServer } from 'apollo-server';
+import { v4 as uuidv4 } from 'uuid';
 
 const typeDefs = gql`
+  type User {
+    id: String!
+    name: String!
+  }
+
   type Query {
-    helloWorld: String!
+    users: [User!]!
+  }
+
+  type Mutation {
+    createUser(name: String!): User!
   }
 `;
 
+interface User {
+  id: string;
+  name: string;
+}
+
+const users: User[] = [];
+
 const server = new ApolloServer({
   typeDefs,
+  resolvers: {
+    Query: {
+      users: () => {
+        return users;
+      },
+    },
+
+    Mutation: {
+      createUser: (_, args) => {
+        const user = {
+          id: uuidv4(),
+          name: args.name,
+        };
+
+        users.push(user);
+
+        return user;
+      },
+    },
+  },
 });
 
 server.listen().then(({ url }) => {
